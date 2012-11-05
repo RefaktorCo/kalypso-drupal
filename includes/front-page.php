631fc2f5987eadf89389688a8da45036b4844f90
RefaktorCo <refaktor.co@gmail.com>
2012-11-05 15:09:20 -0800

    -Bootstrap carousel working, time for option to choose.

--- includes/front-page.php
+++ includes/front-page.php
@@ -1,5 +1,49 @@
 <?php 
 
+function kalypso_bootstrap_slider($page){
+  global $root; 
+  $slide_number = theme_get_setting('slides_number');
+?>
+					<div class="row">
+            <div class="span10">
+              <div id="myCarousel" class="carousel slide">
+              
+						   	<div class="carousel-inner">
+							    <?php $i = '1'; while ($i <= $slide_number) { ?>
+							    <div class="<?php if ($i == '1') {echo "active ";} ?>item">
+							      <a href="<?php echo theme_get_setting('slide_url_'.$i.''); ?>">
+							      	<img src="<?php print file_create_url(theme_get_setting('slide_path_'.$i.'')); ?>">
+							      </a>
+							      <?php if (theme_get_setting('slide_caption_'.$i.'') != '') : ?>
+							    	<div class="carousel-caption">
+								    	<p><?php echo theme_get_setting('slide_caption_'.$i.''); ?></p>
+								    </div><!-- end caption -->
+								    <?php endif; ?>
+							    </div><!-- end item -->
+							    <?php $i++; } ?>
+							  </div>
+							  <!-- end carousel-inner -->
+							
+							  <!-- Carousel nav -->
+							  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+							  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+						  
+						  </div>
+						  <!-- end myCarousel -->            
+						</div>
+				  </div>
+				  
+				  <script type="text/javascript">
+						jQuery(document).ready(function ($) {
+						  $('.carousel').carousel({
+						    interval: 5000
+						  })
+						})
+				  </script>
+          
+<?php }
+
+
 function kalypso_nivo_slider($page){
   global $root; 
   $slide_number = theme_get_setting('slides_number');
@@ -98,7 +142,7 @@ function kalypso_front_page($page){
           </div> 
           <?php } ?>
  
-          <?php if (theme_get_setting('enable_slider') == '1') { kalypso_nivo_slider(); } ?>
+          <?php if (theme_get_setting('enable_slider') == '1') { kalypso_bootstrap_slider(); } ?>
    
           <div class="row">
             <div class="span10">
