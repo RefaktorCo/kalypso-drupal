2e82234408aea5d268759e0571b98a3f0216692e
RefaktorCo <refaktor.co@gmail.com>
2012-11-08 12:06:24 -0800

    -Alright 404 works with no user stuff, will make an option for it in the next version.

--- templates/page--404.tpl.php
+++ templates/page--404.tpl.php
@@ -1,8 +1,9 @@
-<?php kalypso_header($page);?>
+  <div class="container">
+    <div class="row">
         <div class="span10">
         
           <div class="row">
-            <div class="span10">
+            <div class="span12">
               <div id="page_heading">
               <h2 class="page_heading_text"><?php print $title; ?></h2>
               <div id="breadcrumbs"><h3><?php if (theme_get_setting('breadcrumbs') == '1') {print $breadcrumb . $title; } ?></h3></div>
@@ -11,18 +12,18 @@
           </div> 
           
           <div class="row">
-            <div id ="main_content_wrap" class="span10">
+            <div id ="main_content_wrap" class="span12">
               <div id="main_content">
 	              <div class="error_wrap">
 		              <div class="error_img">
-		                <img src="images/404.png" alt="404">
+		                <img src="<?php global $root; echo $root;?>/images/404.png" alt="404">
 		              </div>
 		             
 		              <div class="error_text">
 		                <h2>Page not found</h2>
 		                <p>We're sorry, but the page you are looking for cannot be found. Try one of the following instead:</p>
 		                <br>
-		                <p><a class="btn btn-large" href="index.html"> Home</a> &nbsp;&nbsp;&nbsp;<a class="btn btn-large" href="contact.html"> Contact</a></p>
+		                <p><a class="btn btn-large" href="<?php print base_path();?>"> Home</a></p>
 		              </div>    
 		            </div>
                   </div>
