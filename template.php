a34a83d9b17579eaa37beb4501aed61a6005f68b
RefaktorCo <refaktor.co@gmail.com>
2012-11-08 13:50:39 -0800

    -Looks like I finally fixed the safari tags issue, i'll know when I put it live.

--- template.php
+++ template.php
@@ -36,11 +36,11 @@ function kalypso_field($variables) {
  
   // Render the label, if it's not hidden.
   if (!$variables['label_hidden']) {
-    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+    
   }
  
   // Render the items.
-  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+ 
  
   
   if ($variables['element']['#field_name'] == 'field_tags') {
@@ -66,11 +66,9 @@ function kalypso_field($variables) {
       $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
     }
   }
-  $output .= '</div>';
- 
+  
   // Render the top-level DIV.
-  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
- 
+   
   return $output;
 }
 
