839d15adfed73a19db977fd4204be7a57c024ce5
refaktorco <refaktor.co@gmail.com>
2013-02-26 13:47:46 -0800

    -Adding missing edit tabs where needed

--- templates/page--node--article.tpl.php
+++ templates/page--node--article.tpl.php
@@ -15,7 +15,17 @@
               <div id="main_content">
 	              <?php print render($title_prefix); ?>
 	              <?php print render($title_suffix); ?>
-	              <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+	              <?php if ($tabs = render($tabs)): ?>
+							    <div id="drupal_tabs" class="tabs">
+							    <?php print render($tabs); ?>
+							    </div>
+							  <?php endif; ?>
+					      <?php print render($page['help']); ?>
+					      <?php if ($action_links): ?>
+					        <ul class="action-links">
+					          <?php print render($action_links); ?>
+					        </ul>
+					      <?php endif; ?>
 	              <?php print render($page['content']); ?>
 	            </div>
 	          </div>
